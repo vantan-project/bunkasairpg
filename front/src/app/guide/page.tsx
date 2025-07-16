99951c5c419dcbb3c9a18f1fccb57b0dd0af8f2d
Kate0418 <nakao.keito0418@gmail.com>
2025-07-16 17:28:40 +0900

    微修正

--- front/src/app/guide/page.tsx
+++ front/src/app/guide/page.tsx
@@ -1,5 +1,7 @@
 export default function Page() {
   return (
-    <div className="h-screen flex justify-center items-center text-4xl">4階で勇者のQRを受け取って冒険を始めよう。</div>
+    <div className="h-screen flex justify-center items-center text-4xl">
+      4階で勇者のQRを受け取って冒険を始めよう。
+    </div>
   );
 }
