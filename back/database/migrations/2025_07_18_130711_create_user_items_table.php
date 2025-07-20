24255e2429715a707924005e35eb657468533a3a
tomoto-png <hirosi.tomoto0130@gmail.com>
2025-07-20 19:48:26 +0900

    追加のコミット

--- back/database/migrations/2025_07_18_130711_create_user_items_table.php
+++ back/database/migrations/2025_07_18_130711_create_user_items_table.php
@@ -12,11 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user_items', function (Blueprint $table) {
-            $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('item_id');
+            $table->primary(['user_id', 'item_id']);
             $table->unsignedInteger('count');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
         });
     }
 
