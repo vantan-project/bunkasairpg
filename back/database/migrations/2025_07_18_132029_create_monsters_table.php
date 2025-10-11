c722cb0b713a137e547ecb6cd528b4f24b6e4bd4
Kate0418 <nakao.keito0418@gmail.com>
2025-10-11 23:30:56 +0900

    微修正

--- back/database/migrations/2025_07_18_132029_create_monsters_table.php
+++ back/database/migrations/2025_07_18_132029_create_monsters_table.php
@@ -6,41 +6,41 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('monsters', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->unsignedBigInteger('weapon_id')->nullable();
-            $table->unsignedBigInteger('item_id')->nullable();
-            $table->string('index_number');
-            $table->string('name');
-            $table->text('image_url');
-            $table->unsignedInteger('attack');
-            $table->unsignedInteger('hit_point');
-            $table->unsignedInteger('experience_point');
-            $table->decimal('slash', 2, 1);
-            $table->decimal('blow', 2, 1);
-            $table->decimal('shoot', 2, 1);
-            $table->decimal('neutral', 2, 1);
-            $table->decimal('flame', 2, 1);
-            $table->decimal('water', 2, 1);
-            $table->decimal('wood', 2, 1);
-            $table->decimal('shine', 2, 1);
-            $table->decimal('dark', 2, 1);
-            $table->timestamps();
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('monsters', function (Blueprint $table) {
+      $table->uuid('id')->primary();
+      $table->unsignedBigInteger('weapon_id')->nullable();
+      $table->unsignedBigInteger('item_id')->nullable();
+      $table->string('index_number')->unique();
+      $table->string('name');
+      $table->text('image_url');
+      $table->unsignedInteger('attack');
+      $table->unsignedInteger('hit_point');
+      $table->unsignedInteger('experience_point');
+      $table->decimal('slash', 2, 1);
+      $table->decimal('blow', 2, 1);
+      $table->decimal('shoot', 2, 1);
+      $table->decimal('neutral', 2, 1);
+      $table->decimal('flame', 2, 1);
+      $table->decimal('water', 2, 1);
+      $table->decimal('wood', 2, 1);
+      $table->decimal('shine', 2, 1);
+      $table->decimal('dark', 2, 1);
+      $table->timestamps();
 
-            $table->foreign('weapon_id')->references('id')->on('weapons')->onDelete('set null');
-            $table->foreign('item_id')->references('id')->on('items')->onDelete('set null');
-        });
-    }
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('monsters');
-    }
+      $table->foreign('weapon_id')->references('id')->on('weapons')->onDelete('set null');
+      $table->foreign('item_id')->references('id')->on('items')->onDelete('set null');
+    });
+  }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('monsters');
+  }
 };
