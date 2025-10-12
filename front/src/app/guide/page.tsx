c6dcf0ffa20cb45796f2dbfe8a602fe8bfc66efb
Ryota-Okumuraa <okumurahaku3298@icloud.com>
2025-10-12 22:20:13 +0900

    feature/guideページのレイアウトの作成

--- front/src/app/guide/page.tsx
+++ front/src/app/guide/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
   return (
-    <div className="h-screen flex justify-center items-center text-4xl">
-      4階で勇者のQRを受け取って冒険を始めよう。
+    <div className="h-screen grid place-items-center bg-[url('/guide_background.png')] bg-[length:100%_100%] bg-no-repeat">
+      <h1 className="text-4xl text-center text-yellow-950 font-medium leading-[72px] tracking-[3.04px] [text-shadow:_1px_1px_0_#F8E1CF,_-1px_-1px_0_#F8E1CF,_1px_-1px_0_#F8E1CF,_-1px_1px_0_#F8E1CF]">
+        四階で勇者の<br />QRをもらって<br />冒険を始めよう!
+      </h1>
     </div>
   );
 }
