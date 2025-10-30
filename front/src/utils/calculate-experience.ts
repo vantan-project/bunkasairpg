54e15d2f7d5c4211b15a50cc4e933d3d81da51b9
Kate0418 <nakao.keito0418@gmail.com>
2025-10-30 10:52:51 +0900

    新しいレベル計算式

--- front/src/utils/calculate-experience.ts
+++ front/src/utils/calculate-experience.ts
@@ -1,4 +1,12 @@
 export function calculateExperience(level: number): number {
   if (level <= 1) return 0;
-  return Math.ceil((2.3 * level) ** 2 - 1);
+
+  const BASE_EXP = 19;
+  const RATE_OF_INCREASE = 0.067;
+  
+  const factor = BASE_EXP / RATE_OF_INCREASE;
+  const power = Math.pow(1 + RATE_OF_INCREASE, level - 1);
+  const requiredExperience = factor * (power - 1);
+  
+  return Math.ceil(requiredExperience);
 }
